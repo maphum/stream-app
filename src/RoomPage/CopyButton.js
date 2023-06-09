b9b60f355b972df3821a4cbe48f7c10f0db41f6b
maphum <khanhnam1909@gmail.com>
2023-06-09 22:24:30 +0700

    next test

--- src/RoomPage/CopyButton.js
+++ src/RoomPage/CopyButton.js
@@ -3,10 +3,32 @@ import '../JoinRoomPage/JoinRoomPage.css';
 
 const CopyButton = ({text}) => {
     const [copy, setCopy] = useState(false);
-    const copyFunction = () => {
-        if (text) navigator.clipboard.writeText(text);
-        setCopy(true);
+    const  copyFunction = async () => {
+        if (navigator.clipboard && window.isSecureContext) {
+            await navigator.clipboard.writeText(text);
+          } else {
+            const textarea = document.createElement('textarea');
+            textarea.value = text;
+        
+            // Move the textarea outside the viewport to make it invisible
+            textarea.style.position = 'absolute';
+            textarea.style.left = '-99999999px';
+        
+            document.body.prepend(textarea);
+        
+            // highlight the content of the textarea element
+            textarea.select();
+        
+            try {
+              document.execCommand('copy');
+            } catch (err) {
+              console.log(err);
+            } finally {
+              textarea.remove();
+            }
     }
+    setCopy(true)
+}
     return (               
     <div className='button_copy_container'>
         <button className='button_copy' onClick={copyFunction}>
